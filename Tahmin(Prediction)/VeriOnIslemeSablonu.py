01be550699b2f7ac37adf84aecedd6f0eaffb13a
Yasin Baran Kaplan <101335414+YasinBaranKaplan@users.noreply.github.com>
2023-12-09 16:49:27 +0300

    Update VeriOnIslemeSablonu.py

--- Tahmin(Prediction)/VeriOnIslemeSablonu.py
+++ Tahmin(Prediction)/VeriOnIslemeSablonu.py
@@ -24,4 +24,4 @@ x_train,x_test,y_train,y_test=train_test_split(aylar,satislar,test_size=0.33,ran
 from sklearn.preprocessing import StandardScaler
 sc=StandardScaler()
 X_train=sc.fit_transform(x_train)
-x_test=sc.fit_transform(x_test)
+X_test=sc.fit_transform(x_test)
