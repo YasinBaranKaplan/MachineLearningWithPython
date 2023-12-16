8d2446a0d99caa1228be65c74fd87c8b5f231870
YasinBaranKaplan <kaplanyasin2001@gmail.com>
2023-12-16 21:23:31 +0300

    Create DecisionTree.py

---
+++ Tahmin(Prediction)/Polinomal Regresyon/DecisionTree.py
@@ -0,0 +1,103 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Dec 15 16:51:33 2023
+
+@author: kapla
+"""
+
+import numpy as np 
+import matplotlib.pyplot as plt
+import pandas as pd
+
+#veri yükleme
+veriler = pd.read_csv("maaslar.csv")
+
+x=veriler.iloc[:,1:2]
+y=veriler.iloc[:,2:]
+X=x.values
+Y=y.values
+
+#Liear Regression
+from sklearn.linear_model import LinearRegression
+lin_reg = LinearRegression()
+lin_reg.fit(X,Y)
+
+linearpredict= lin_reg.predict(x)
+
+plt.scatter(X,Y,color='red')
+plt.plot(X,linearpredict,color='blue')
+plt.show()
+
+#polynomial regression  // nonlineer model oluşturma
+from sklearn.preprocessing import PolynomialFeatures
+poly_reg = PolynomialFeatures(degree = 2)
+x_poly = poly_reg.fit_transform(X)
+lin_reg2 = LinearRegression()
+lin_reg2.fit(x_poly,Y)#
+plt.scatter(X,Y,color='red')
+plt.plot(X,lin_reg2.predict(poly_reg.fit_transform(X)),color='blue')
+plt.show()
+
+#degree arttıkça tahmin gerçek sonuçlara yaklaşır.
+from sklearn.preprocessing import PolynomialFeatures
+poly_reg = PolynomialFeatures(degree = 4)
+x_poly = poly_reg.fit_transform(X)
+lin_reg2 = LinearRegression()
+lin_reg2.fit(x_poly,Y)#
+plt.scatter(X,Y,color='red')
+plt.plot(X,lin_reg2.predict(poly_reg.fit_transform(X)),color='blue')
+plt.show()
+
+
+#Scale etmek önemli çünkü support vector reg'in dezavantajı aykırı verilerdir. scale etmek öçleklenmdirmedir.
+from sklearn.preprocessing import StandardScaler
+sc1=StandardScaler()
+x_olcekli=sc1.fit_transform(X)
+sc2=StandardScaler()
+y_olcekli=sc2.fit_transform(Y)
+
+
+from sklearn.svm import SVR
+svr_reg=SVR(kernel = 'rbf')
+svr_reg.fit(x_olcekli,y_olcekli)
+
+plt.scatter(x_olcekli, y_olcekli,color = 'red')
+plt.plot(x_olcekli,svr_reg.predict(x_olcekli),color='blue')
+plt.show()
+
+#Decissiontree
+from sklearn.tree import DecisionTreeRegressor
+r_dt = DecisionTreeRegressor(random_state=0)
+r_dt.fit(X,Y)
+
+plt.scatter(X, Y,color ='red')
+plt.plot(X,r_dt.predict(X))
+plt.show()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
