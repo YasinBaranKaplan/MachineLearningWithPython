f40f92981a65be94539982876be1b9d6d0dde2ec
YasinBaranKaplan <kaplanyasin2001@gmail.com>
2023-12-21 18:47:30 +0300

    Create SupportVectorClassification.py

---
+++ Sınıflandırma (Classification)/SupportVectorClassification.py
@@ -0,0 +1,74 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Nov 12 20:03:58 2023
+
+@author: kapla
+"""
+#1.Kütüphaneler
+import numpy as np
+import matplotlib.pyplot as plt
+import pandas as pd
+
+veriler = pd.read_csv("veriler.csv")
+
+x = veriler.iloc[:,1:4].values#bağımsız değişkenler
+y=veriler.iloc[:,4:]#bağımlı değişken
+
+
+from sklearn.model_selection import train_test_split
+x_train,x_test,y_train,y_test=train_test_split(x,y,test_size=0.33,random_state=0)
+
+
+from sklearn.linear_model import LogisticRegression
+logr=LogisticRegression()
+logr.fit(x_train, y_train)
+
+y_predict = logr.predict(x_test)
+
+from sklearn.metrics import confusion_matrix
+cm=confusion_matrix(y_test, y_predict)
+print(cm)
+
+#KNN 
+from sklearn.neighbors import KNeighborsClassifier
+knn=KNeighborsClassifier(n_neighbors=1,metric='minkowski')
+
+knn.fit(x_train,y_train)
+y_pred_knn=knn.predict(x_test)
+cm_knn=confusion_matrix(y_test, y_pred_knn)
+print(cm)
+
+#Support Vector Classification
+from sklearn.svm import SVC
+svc=SVC(kernel='linear')
+svc.fit(x_test, y_test)
+y_pred_svc=svc.predict(x_test)
+cm_svc=confusion_matrix(y_test, y_pred_svc)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
